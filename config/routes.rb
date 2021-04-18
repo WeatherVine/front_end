cbf86e942c2ff6df7df29a4fdb001e4d4b05dfe0
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-18 11:26:47 -0600

    Add discover wines button to user dashboard
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>
    Co-authored-by: Diana Buffone <71909590+Diana20920@users.noreply.github.com>
    Co-authored-by: Katy La Tour <23460878+klatour324@users.noreply.github.com>
    Co-authored-by: Trevor Suter <72848529+Trevorsuter@users.noreply.github.com>
    Co-authored-by: Wil McCauley <64031242+Wil-McC@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get '/user/dashboard', to: 'dashboard#show'
+
+  #discover
+  resources :discover, only: [:index]
 end
---
+++ spec/features/dashboard/user_dashboard_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'User Dashboard Spec', type: :feature do
+  describe 'happy path' do
+    before :each do
+      stub_omniauth
+
+      visit "/"
+
+      click_button 'Register/Login with Google', match: :first
+    end
+
+      describe 'as a user, when I vist my user dashboard' do
+
+        it 'has a button to discover wines, a button to logout, and a section for my favorite wines' do
+          within(".nav-bar") do
+            expect(page).to have_button("Discover Wines")
+            # expect(page).to have_button("Logout")
+          end
+          # expect(page).to have_content("My Favorite Wines")
+
+        end
+      end
+  end
+end
+
+    # it 'logs a returning user in via google mock' do
+    #   stub_omniauth
+    #   user = create(:omniauth_mock_user)
+    #   user_count = User.count
+    #   expect(user_count).to eq(1)
+    #
+    #   visit root_path
+    #
+    #   click_link 'Log in with Google'
+    #
+    #   user_count = User.count
+    #   expect(user_count).to eq(1)
+    #
+    #   expect(current_path).to eq(dashboard_path)
+    #   expect(page).to have_content("Welcome back, #{user.username}")
+    # end

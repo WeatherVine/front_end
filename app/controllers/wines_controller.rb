963fce99c600f0bc19f79e4feb7480654682419a
Adam Bowers <adam.j.bowers@gmail.com>
2021-04-20 21:08:04 -0600

    Refactor to allow only one call to facade.
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>
    Co-authored-by: Tommy Nieuwenhuis <72888891+tsnieuwen@users.noreply.github.com>

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,8 +2,10 @@ class WinesController < ApplicationController
   before_action :authorized, only: [:show]
 
   def show
-    @wine = WinesService.wine(params[:id])
-    @wines_api_ids = FindWineService.wine_api_ids(current_user.id)
+    @wine = WinesFacade.return(params[:id], current_user.id)
+    # require "pry"; binding.pry
+    # @wine = WinesService.wine(params[:id])
+    # @wines_api_ids = FindWineService.wine_api_ids(current_user.id)
   end
 
 end

3bbb8898ec7d0ced75b462a0e12fba4f5dc51911
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-04-21 08:21:47 -0600

    Sad path testing for create and delete favorite wines actions
    Co-authored-by: Adam Bowers <69491049+Pragmaticpraxis37@users.noreply.github.com>
    Co-authored-by: Alex Schwartz <14796798+aschwartz1@users.noreply.github.com>

--- app/controllers/user_wines_controller.rb
+++ app/controllers/user_wines_controller.rb
@@ -1,8 +1,17 @@
 class UserWinesController < ApplicationController
 
   def destroy
-    Faraday.delete("https://weathervine-be.herokuapp.com/api/v1/user/#{current_user.id}/wines/#{params[:id]}")
-    redirect_to user_dashboard_path
+    resp = Faraday.delete("https://weathervine-be.herokuapp.com/api/v1/user/#{current_user.id}/wines/#{params[:id]}") do |req|
+      req.params['user_id'] = current_user.id
+      req.params['wine_id'] = params[:id]
+    end
+    
+    if resp.status == 200
+      redirect_to user_dashboard_path
+    else
+      flash[:message] = "We're sorry, there was an issue with your request"
+      redirect_to user_dashboard_path
+    end
   end
 
   def create

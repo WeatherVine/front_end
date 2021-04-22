be637fe4dc52bfe6c346366c4ce66e0e3fe25126
Alex Schwartz <aschwartz@detaso.com>
2021-04-22 17:24:16 -0500

    Reformat show spec to user vcr where possible
    Co-authored-by: Katy La Tour <23460878+klatour324@users.noreply.github.com>

--- spec/features/wines/show_spec.rb
+++ spec/features/wines/show_spec.rb
@@ -3,25 +3,76 @@ require 'webmock/rspec'
 
 RSpec.describe 'Wine Show Page Spec', type: :feature do
   describe 'happy path' do
-    before :each do
-      stub_omniauth
+    # before :each do
+    #   stub_omniauth
 
-      @user = create(:user)
+    #   @user = create(:user)
+    #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    #   single_wine_json_response = File.read('spec/fixtures/wine_show.json')
+    #   wines_json_response = File.read('spec/fixtures/users_wines.json')
+
+    #   stub_request(:get, "https://weathervine-be.herokuapp.com/api/v1/wines/546e64cf4c6458020000000d").
+    #     with(
+    #       headers: {
+    #      'Accept'=>'*/*',
+    #      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #      'User-Agent'=>'Faraday v1.3.0'
+    #       }).
+    #     to_return(status: 200, body: "#{single_wine_json_response}", headers: {})
+
+    #   stub_request(:get, "https://weathervine-be.herokuapp.com/api/v1/users/#{@user.id}/dashboard").
+    #     with(
+    #       headers: {
+    #      'Accept'=>'*/*',
+    #      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #      'User-Agent'=>'Faraday v1.3.0'
+    #       }).
+    #     to_return(status: 200, body: "#{wines_json_response}", headers: {})
+
+    #   stub_request(:post, "https://weathervine-be.herokuapp.com/api/v1/user/#{@user.id}/wines?comment=&name=Duckhorn%20Sauvignon%20Blanc&user_id=#{@user.id}&wine_id=546e64cf4c6458020000000d").
+    #     with(
+    #     headers: {
+    #       'Accept'=>'*/*',
+    #       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+    #       'Content-Length'=>'0',
+    #       'User-Agent'=>'Faraday v1.3.0'
+    #       }).
+    #       to_return(status: 200, body: "", headers: {})
+
+    #   visit "/wines/546e64cf4c6458020000000d"
+    # end
+
+    it "shows page" do
+      @user = create(:user, id: 111)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      single_wine_json_response = File.read('spec/fixtures/wine_show.json')
+      VCR.use_cassette('wine_show_page') do
+        visit "/wines/546e64cf4c6458020000000d"
+
+        expect(page).to have_content("Duckhorn")
+        expect(page).to have_content("Napa Valley")
+        expect(page).to have_content("2013")
+        # expect(page).to have_content("")
+        expect(page).to have_content("Citrus, earthy aromas")
+        expect(page).to have_content("Citrus, earthy flavours, fresh acidity, warm alcohol")
+        expect(page).to have_content("Medium duration, good quality, middle peaktime")
+        expect(page).to have_content("Subtle complexity, pleasant interest, harmonious balance")
+        expect(page).to have_content("81")
+        expect(page).to have_content("79.0")
+        expect(page).to have_content("2012-01-01")
+        expect(page).to have_content("2012-12-31")
+        expect(page).to have_button("Add Wine to Favorite List")
+      end
+    end
+
+    it "adds wine to favorite list" do
+      @user = create(:user, id: 111)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      # Request used to get intial state of user's favorite wines
       wines_json_response = File.read('spec/fixtures/users_wines.json')
-
-      stub_request(:get, "https://weathervine-be.herokuapp.com/api/v1/wines/546e64cf4c6458020000000d").
-        with(
-          headers: {
-         'Accept'=>'*/*',
-         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-         'User-Agent'=>'Faraday v1.3.0'
-          }).
-        to_return(status: 200, body: "#{single_wine_json_response}", headers: {})
-
-      stub_request(:get, "https://weathervine-be.herokuapp.com/api/v1/users/#{@user.id}/dashboard").
+      stub_request(:get, "#{ENV['BACK_END_URL']}/api/v1/users/#{@user.id}/dashboard").
         with(
           headers: {
          'Accept'=>'*/*',
@@ -30,7 +81,19 @@ RSpec.describe 'Wine Show Page Spec', type: :feature do
           }).
         to_return(status: 200, body: "#{wines_json_response}", headers: {})
 
-      stub_request(:post, "https://weathervine-be.herokuapp.com/api/v1/user/#{@user.id}/wines?comment=&name=Duckhorn%20Sauvignon%20Blanc&user_id=#{@user.id}&wine_id=546e64cf4c6458020000000d").
+      # Request to get the wine show page
+      single_wine_json_response = File.read('spec/fixtures/wine_show.json')
+      stub_request(:get, "#{ENV['BACK_END_URL']}/api/v1/wines/546e64cf4c6458020000000d").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.3.0'
+          }).
+        to_return(status: 200, body: "#{single_wine_json_response}", headers: {})
+
+      # Request to add to a user's favorite wines
+      stub_request(:post, "#{ENV['BACK_END_URL']}/api/v1/user/#{@user.id}/wines?comment=&name=Duckhorn%20Sauvignon%20Blanc&user_id=#{@user.id}&wine_id=546e64cf4c6458020000000d").
         with(
         headers: {
           'Accept'=>'*/*',
@@ -41,26 +104,10 @@ RSpec.describe 'Wine Show Page Spec', type: :feature do
           to_return(status: 200, body: "", headers: {})
 
       visit "/wines/546e64cf4c6458020000000d"
-    end
 
-    it "shows page" do
-      expect(page).to have_content("Duckhorn Sauvignon Blanc")
-      expect(page).to have_content("Napa Valley")
-      expect(page).to have_content("2018")
-      expect(page).to have_content("Eagle")
-      expect(page).to have_content("Citrus, earthy aromas")
-      expect(page).to have_content("Citrus, earthy flavours, fresh acidity, warm alcohol")
-      expect(page).to have_content("Medium duration, good quality, middle peaktime")
-      expect(page).to have_content("Subtle complexity, pleasant interest, harmonious balance")
-      expect(page).to have_content("75")
-      expect(page).to have_content("100")
-      expect(page).to have_button("Add Wine to Favorite List")
-    end
-
-    it "adds wine to favorite list" do
+      # Request to get the _new_ (after having added a wine) user dashboard page
       added_favorite_wine_response = File.read('spec/fixtures/user_wines_after_create.json')
-
-      stub_request(:get, "https://weathervine-be.herokuapp.com/api/v1/users/#{@user.id}/dashboard").
+      stub_request(:get, "#{ENV['BACK_END_URL']}/api/v1/users/#{@user.id}/dashboard").
         with(
           headers: {
          'Accept'=>'*/*',
